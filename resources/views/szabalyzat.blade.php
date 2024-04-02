e161191044394e249114ee020c66e1287ff83602
F.Dávid <farkasdavid776@gmail.com>
2024-04-02 23:12:00 +0200

    2 példa kártya a szabályzatokhoz
    2 kártyát töltöttem fel a szabalyzat.blade.php-ba.
    
    2 példa kártyát.

--- resources/views/szabalyzat.blade.php
+++ resources/views/szabalyzat.blade.php
@@ -2,7 +2,28 @@
 
 @section('content')
 
-<h1 class="text-center mt-10 anima">Játékszabályzat</h1>
+<h1 class="text-center mt-10 anima">Lethal Omen Játékszabályzat</h1>
+<h4 class="text-center mt-10 anima">A Lethal Omen egy a Yu-Gi-Oh!, Magic: the Gathering-hez hasonló "Trading Card Game" varázskártyákkal és szörny kártyákkal. </h4>
+<h4 class="text-center mt-10 anima">A kártyákat két nagy csoportba tudjuk sorolni, a szörnyek osztálya és a varázslapok osztálya.</h4>
+
+<div class="text-center mt-10">
+    <img src="{{asset('img/kartyak/Battle of Legends.png')}}" class="img-fluid kepmeretezes">
+    <img src="{{asset('img/kartyak/Witch Hunter.png')}}" class="img-fluid kepmeretezes">
+</div>
+
+{{--<div class="text-center mt-10" id="cardContainer">
+    <div class="interactive-card" id="card1">
+        <img src="{{asset('img/kartyak/Battle of Legends.png')}}" class="img-fluid kepmeretezes">
+    </div>
+    <div class="interactive-card" id="card2">
+        <img src="{{asset('img/kartyak/Witch Hunter.png')}}" class="img-fluid kepmeretezes">
+    </div>
+</div>--}}
+
+
+
+
+
 
 <div class="wrapper"></div>
 
