1516cb623fe58f36da331124dc83502a2f439c76
Semencki Dávid <57768850+davidvok-coder@users.noreply.github.com>
2024-04-11 20:32:21 +0200

    Főgomb link
    A feliratot átalakítottuk főgombra is.
    
    Co-Authored-By: F.Dávid <118255717+Judzsin@users.noreply.github.com>
    Co-Authored-By: Dracula <44154833+666Dracula999@users.noreply.github.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     
 <nav class="navbar navbar-expand-lg navbar-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="{{ url('/omen') }}">
             <img src="{{asset('img/Logo.png')}}" class="img-fluid">Lethal Omen
         </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
