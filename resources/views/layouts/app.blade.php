3911f2c6240489d7b58a1878cd144e368c110f2e
Semencki Dávid <57768850+davidvok-coder@users.noreply.github.com>
2024-04-11 22:29:45 +0200

    audio nelkul
    Co-Authored-By: F.Dávid <118255717+Judzsin@users.noreply.github.com>
    Co-Authored-By: Dracula <44154833+666Dracula999@users.noreply.github.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,13 +49,8 @@
             @yield('content')
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </div>
-    <audio src="sound.mp3"  autoplay loop id="music"></audio>
-    <script>
-   
-        function playAudio() {
-    var audio = document.getElementById("music");
-    audio.play();
-  }
+    
+  
 </script>
 </body>
 </html>
