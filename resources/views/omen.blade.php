d48b7d2155f87b994c1faa583ef65584ad91d74b
F.Dávid <farkasdavid776@gmail.com>
2024-04-25 21:07:35 +0200

    Omen főoldal szerkesztése
    Co-Authored-By: Dracula <44154833+666Dracula999@users.noreply.github.com>
    Co-Authored-By: Semencki Dávid <57768850+davidvok-coder@users.noreply.github.com>

--- resources/views/omen.blade.php
+++ resources/views/omen.blade.php
@@ -5,14 +5,13 @@
 <div class="container mt-5 p-4">
     <div class="row justify-content-center">
         <div class="col-md-6">
+
             <div class="input-group mb-3">
                 <input type="search" class="form-control" placeholder="Keresés..." aria-label="Keresés..." aria-describedby="button-addon2">
-                <button class="btn btn-outline-secondary" type="button" id="button-addon2">Keresés</button>
-
-
-                
+                <button class="btn btn-outline-secondary" type="button" id="button-addon2">Keresés</button>                
 
             </div>
+
         </div>
     </div>
 </div>
@@ -26,17 +25,27 @@
             </div>
             <div class="card-wrapper"> 
         @endif
+
         <div class="cards">
-            
-            <img src="{{ asset('storage/kartyak/'.$kartya->Kártyakép) }}" class="card-image">
-            <p class="card-price">{{$kartya->Ár}} Lethal Coin</p>
-            
-            
+
+            <div class="card-box">
+                <img src="{{ asset('storage/kartyak/'.$kartya->Kártyakép) }}" class="card-image">
+            </div>
+
+            <br>
+            <div class="price-box">
+                <p class="card-price">{{$kartya->Ár}} Lethal Coin <p class="kosar-ikon">🛒</p></p>
+            </div>
+                        
         </div>
+
+
     @endforeach
 </div>
 
+<br>
 
+   
 
 
 
